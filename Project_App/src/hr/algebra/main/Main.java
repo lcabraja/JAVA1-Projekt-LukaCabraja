c6224e2c24080e68538be8eca3ee59ea2abe9ccb
Luka Cabraja <luka@cabraja.eu>
2021-09-09 11:04:12 +0200

    log in support

--- Project_App/src/hr/algebra/main/Main.java
+++ Project_App/src/hr/algebra/main/Main.java
@@ -5,9 +5,12 @@
  */
 package hr.algebra.main;
 
+import hr.algebra.admin.CinestarDownload;
 import hr.algebra.login.Login;
-import hr.algebra.utils.MessageUtils;
-import javax.swing.JDialog;
+import hr.algebra.model.User;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JMenuItem;
 
 /**
  *
@@ -23,6 +26,9 @@ public class Main extends javax.swing.JFrame {
         init();
     }
 
+    private User user = null;
+    private CinestarDownload cinestarDownload;
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -33,60 +39,39 @@ public class Main extends javax.swing.JFrame {
     private void initComponents() {
 
         jTabbedPane1 = new javax.swing.JTabbedPane();
-        jPanel1 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
-        jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem5 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 824, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 579, Short.MAX_VALUE)
-        );
-
-        jTabbedPane1.addTab("tab1", jPanel1);
+        setMinimumSize(new java.awt.Dimension(800, 600));
+        setPreferredSize(new java.awt.Dimension(800, 600));
+        setResizable(false);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         jMenu1.setText("File");
 
         jMenuItem1.setText("jMenuItem1");
         jMenu1.add(jMenuItem1);
 
-        jCheckBoxMenuItem1.setSelected(true);
-        jCheckBoxMenuItem1.setText("jCheckBoxMenuItem1");
-        jMenu1.add(jCheckBoxMenuItem1);
+        jMenuItem6.setText("Exit");
+        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem6ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem6);
 
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("User");
-        jMenu2.add(jMenuItem2);
-
-        jMenuItem3.setText("jMenuItem3");
-        jMenu2.add(jMenuItem3);
-
-        jMenuItem4.setText("jMenuItem4");
-        jMenu2.add(jMenuItem4);
-
-        jMenuItem5.setText("Log Out");
-        jMenuItem5.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jMenuItem5MouseClicked(evt);
-            }
-        });
-        jMenu2.add(jMenuItem5);
-
+        jMenu2.setEnabled(false);
         jMenuBar1.add(jMenu2);
 
         setJMenuBar(jMenuBar1);
@@ -105,9 +90,13 @@ public class Main extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem5MouseClicked
-        tabsLogin();
-    }//GEN-LAST:event_jMenuItem5MouseClicked
+    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jMenuItem6ActionPerformed
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+
+    }//GEN-LAST:event_formWindowOpened
 
     /**
      * @param args the command line arguments
@@ -120,20 +109,16 @@ public class Main extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Metal".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+
         //</editor-fold>
 
         /* Create and display the form */
@@ -145,37 +130,83 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JTabbedPane jTabbedPane1;
     // End of variables declaration//GEN-END:variables
 
     private void init() {
         setLocationRelativeTo(null);
         setTitle("JAVA1DB");
-        
-        jMenuItem2.setText("Username: ");
-        jMenuItem3.setText("Role: ");
-        jMenuItem4.setText("Access Level: ");
+
+        setUserMenu();
         createPanels();
-        tabsLogin();
+        setLoginTabs();
     }
-    
+
+    private void setUserMenu() {
+        //        try {
+//            List<Movie> parse = MovieParser.parse();
+//            parse.isEmpty();
+//            parse.forEach((movie) -> {
+//                System.out.println(movie);
+//            });
+//        } catch (IOException | XMLStreamException ex) {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        if (user == null) {
+            return;
+        }
+        jMenu2.setEnabled(true);
+
+        JMenuItem m2Username = new JMenuItem("User: " + user.getUsername());
+        JMenuItem m2Role = new JMenuItem("Role: " + user.getRole());
+        JMenuItem m2AccessLevel = new JMenuItem("Access Level: " + user.getAccessLevel());
+        JMenuItem m2LogOut = new JMenuItem("Log Out");
+
+        m2LogOut.addActionListener((ActionEvent ev) -> {
+            jMenu2.setEnabled(false);
+            setLoginTabs();
+        });
+
+        jMenu2.removeAll();
+        jMenu2.add(m2Username);
+        jMenu2.add(m2Role);
+        jMenu2.add(m2AccessLevel);
+        jMenu2.add(m2LogOut);
+    }
+
     private javax.swing.JPanel loginForm;
-    
+
     private void createPanels() {
         loginForm = new Login(this);
     }
-    
-    private void tabsLogin() {
+
+    private void setLoginTabs() {
+        jTabbedPane1.removeAll();
         jTabbedPane1.addTab("Login", loginForm);
+    }
+
+    public void userLoggedIn(User user) {
+        this.user = user;
+        setUserMenu();
+
+        if ("Admin".equals(user.getRole())) {
+            setAdminTabs();
+        } else {
+            setUserTabs();
+        }
+    }
+
+    private void setAdminTabs() {
+        jTabbedPane1.removeAll();
+        jTabbedPane1.addTab("Cinestar RSS", cinestarDownload);
+    }
+
+    private void setUserTabs() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
