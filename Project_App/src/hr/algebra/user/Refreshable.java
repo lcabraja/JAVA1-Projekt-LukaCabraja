d4c41055ad25544860bdd8e178a2b65d843348cb
Luka Cabraja <luka@cabraja.eu>
2021-09-10 20:02:44 +0200

    created refreshable interface

---
+++ Project_App/src/hr/algebra/user/Refreshable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.algebra.user;
+
+/**
+ *
+ * @author lcabraja
+ */
+public interface Refreshable {
+    
+    public void RefreshData();
+}
